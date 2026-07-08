c640e522ed3c049e72de4b72975960457f59e0d6
sudo-learner <rosn7878@gmail.com>
2026-07-08 22:38:25 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,27 +28,7 @@ export const metadata = {
     },
   ],
 },
-icons: {
-  icon: [
-    {
-      url: "/OpenRead/icons/icon-192.png",
-      sizes: "192x192",
-      type: "image/png",
-    },
-    {
-      url: "/OpenRead/icons/icon-512.png",
-      sizes: "512x512",
-      type: "image/png",
-    },
-  ],
-  apple: [
-    {
-      url: "/OpenRead/icons/icon-192.png",
-      sizes: "192x192",
-      type: "image/png",
-    },
-  ],
-}
+ 
   appleWebApp: {
     capable: true,
     statusBarStyle: "black-translucent",
